fee948f4529a5e674609dc6f860215a0c5e8578c
jachinamhome <yirimirang@jachinam.com>
2025-06-16 19:12:15 +0900

    Initial commit

--- src/components/map/RegionMap.jsx
+++ src/components/map/RegionMap.jsx
@@ -1178,7 +1178,7 @@ const RegionMap = () => {
                             {/* 정렬 바 */}
               <div className="sticky top-0 bg-white/95 backdrop-blur-sm p-4 border-b border-gray-100 z-10 shadow-sm flex flex-row justify-between items-center gap-2 w-full">
                 <div className="flex flex-row items-center font-bold justify-between gap-4 px-4 pb-4">
-                  <span className="text-black text-sm font-light whitespace-nowrap">{filteredVideos.length}개 집을 찾았어요</span>
+                  <span className="text-gray-500 text-sm whitespace-nowrap">{filteredVideos.length}개 집을 찾았어요</span>
                 </div>
                 <div className="flex flex-row gap-2 flex-shrink-0 justify-end">
                   <button
@@ -1416,9 +1416,9 @@ const RegionMap = () => {
               {/* 정렬 바 */}
               <div className="sticky top-0 bg-white/95 backdrop-blur-sm p-4 border-b border-gray-100 z-10 shadow-sm">
                 <div className="flex flex-row items-center justify-between gap-4 w-full flex-nowrap">
-                  <h3 className="text-black text-sm font-bold text-gray-500 whitespace-nowrap flex-shrink-0">
+                  <h3 className="text-gray-500 text-sm font-bold text-gray-500 whitespace-nowrap flex-shrink-0">
                     {/* {selectedAreas.length > 0 ? `${selectedAreas.join(', ')}` : '전체'} */}
-                    <span className="text-black text-sm font-light ml-2">
+                    <span className="ml-2">
                       {filteredVideos.length}개의 집을 찾았어요
                     </span>
                   </h3>

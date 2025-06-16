b3b3270fa8e30d93d49aa27d9d6f311a58064222
jachinamhome <yirimirang@jachinam.com>
2025-06-17 01:26:09 +0900

    Initial commit

--- src/components/map/RegionMap.jsx
+++ src/components/map/RegionMap.jsx
@@ -1074,7 +1074,7 @@ const RegionMap = () => {
               {/* 오른쪽 상단: 필터/키워드/지역선택 버튼 */}
               <div className="absolute top-4 left-1/2 -translate-x-1/2 z-50 flex gap-2 items-center">
                 <Button
-                  className="bg-black text-white border border-black rounded-full w-24 h-9 px-4 sm:px-6 shadow-lg font-semibold flex items-center hover:bg-gray-700 transition-all hover:shadow-xl whitespace-nowrap text-sm"
+                  className="bg-black text-white border border-black rounded-full w-22 h-9 px-4 sm:px-6 shadow-lg font-semibold flex items-center hover:bg-gray-700 transition-all hover:shadow-xl whitespace-nowrap text-sm"
                   onClick={() => setRegionId(null)}
                 >
                   <MdOutlineMap className="w-5 h-5" />
@@ -1082,10 +1082,10 @@ const RegionMap = () => {
                 </Button>
                 <button
                   onClick={() => handleModalButtonClick('region')}
-                  className="bg-black text-white border border-black rounded-full w-24 h-9 px-4 sm:px-6 shadow-lg font-semibold flex items-center hover:bg-gray-50 transition-all hover:shadow-xl whitespace-nowrap text-sm"
+                  className="bg-black text-white border border-black rounded-full w-22 h-9 px-4 sm:px-6 shadow-lg font-semibold flex items-center hover:bg-gray-50 transition-all hover:shadow-xl whitespace-nowrap text-sm"
                 >
                   {nationRegionList.find(r => r.id === decodeURIComponent(regionId))?.label || '지역'}
-                  <FaChevronDown className="w-4 h-4 ml-4" />
+                  <FaChevronDown className="w-4 h-4 ml-3" />
                 </button>
                 <button
                   onClick={() => handleModalButtonClick('filter')}

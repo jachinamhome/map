a503a233f071375436fe2f27676153e95e7af68e
jachinamhome <yirimirang@jachinam.com>
2025-06-17 17:08:13 +0900

    Initial commit

--- src/components/map/RegionMap.jsx
+++ src/components/map/RegionMap.jsx
@@ -1200,11 +1200,11 @@ useEffect(() => {
             >
               {/* 리사이저 */}
               <div
-                className="w-full h-24 flex items-center justify-center cursor-row-resize sticky top-0 z-50 bg-white"
+                className="w-full h-8 flex items-center justify-center sticky top-0 z-50"
                 onMouseDown={handleResizerMouseDown}
                 onTouchStart={handleResizerMouseDown}
               >
-                <div className="w-14 h-1.5 bg-gray-200 rounded-full shadow-sm" />
+                <div className="w-10 h-1.5 bg-gray-300 rounded-full" />
               </div>
 
               {/* 상단 고정 통합 영역 */}
